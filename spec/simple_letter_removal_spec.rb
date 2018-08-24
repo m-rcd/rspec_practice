962f82328548c37306e9cb517b775853ee4df1c1
Marianne Rachid <marianne.rachid@gmail.com>
2018-08-24 17:55:01 +0100

    Removes k characters from string, from a to z, leftmost first

--- spec/simple_letter_removal_spec.rb
+++ spec/simple_letter_removal_spec.rb
@@ -4,6 +4,15 @@ describe 'solve' do
   it 'removes k number of letter a from string' do
     expect(solve("abracadabra", 1)).to eq "bracadabra"
     expect(solve("abracadabra", 2)).to eq "brcadabra"
+    expect(solve("abracadabra", 3)).to eq "brcdabra"
+    expect(solve("abracadabra", 4)).to eq "brcdbra"
+    expect(solve("abracadabra", 5)).to eq "brcdbr"
   end
-
+  it 'removes "k minus number of a" of b' do
+    expect(solve("abracadabra", 6)).to eq "rcdbr"
+    expect(solve("abracadabra", 7)).to eq "rcdr"
+  end
+  it 'removes a, then b then c etc. for k characters' do
+    expect(solve("abracadabra", 50)).to eq ''
+  end
 end
